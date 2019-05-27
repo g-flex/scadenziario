6507dfdc96c93f056ff7d2144f62837a5936ae7d
fabio.pergamiferoldi@gmail.com <fabio.pergamiferoldi@gmail.com>
2019-05-27 15:36:55 +0200

    pagine login e registrati convertite in react

---
+++ login.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+class Login extends React.Component {
+  render() {
+    return(
+      <>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <img src="" alt="logo" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12"><h1>Log in</h1></div>
+            <div class="col-12">
+              <form>
+                <div class="form-group d-flex justify-content-center">
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                </div>
+                <div class="form-group d-flex justify-content-center">
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+                <button type="submit" class="btn btn-primary">Log in</button>
+                <div><small>or</small></div>
+                <div><button class="btn btn-primary"><a href="create_account.html">Create account</a></button></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+export default Login;
+    // <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    //         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    //         crossorigin="anonymous" />
+    // <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" />
+    // <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" />
