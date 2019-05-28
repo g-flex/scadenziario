7df50a5d6c66f59c9a14588f712510a3de5f7ec0
g-flex <ggussoni97@gmail.com>
2019-05-28 14:43:16 +0200

    add style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,25 +5,25 @@ class Login extends React.Component {
   render() {
     return(
       <>
-        <div class="container">
-          <div class="row">
-            <div class="col">
+        <div className="container">
+          <div className="row">
+            <div className="col">
               <img src="" alt="logo" />
             </div>
           </div>
-          <div class="row">
-            <div class="col-12"><h1>Log in</h1></div>
-            <div class="col-12">
+          <div className="row">
+            <div className="col-12"><h1>Log in</h1></div>
+            <div className="col-12">
               <form>
-                <div class="form-group d-flex justify-content-center">
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                <div className="form-group d-flex justify-content-center">
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                 </div>
-                <div class="form-group d-flex justify-content-center">
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <div className="form-group d-flex justify-content-center">
+                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                 </div>
-                <button type="submit" class="btn btn-primary">Log in</button>
+                <button type="submit" className="btn btn-primary">Log in</button>
                 <div><small>or</small></div>
-                <div><button class="btn btn-primary"><Link to="/register">Create account</Link></button></div>
+                <div><button className="btn btn-primary"><Link to="/register">Create account</Link></button></div>
               </form>
             </div>
           </div>
@@ -33,8 +33,3 @@ class Login extends React.Component {
   }
 }
 export default Login;
-    //<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    //         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    //         crossorigin="anonymous" />
-    // <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" />
-    // <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,32 +4,32 @@ class Register extends React.Component {
   render() {
     return(
       <>
-        <div class="container">
-        <div class="row">
-           <div class="col">
+        <div className="container">
+        <div className="row">
+           <div className="col">
             <img src={logo} height='200' />
            </div>
         </div>
-        <div class="row">
-           <div class="col-12"><h1>Create Account</h1></div>
-           <div class="col-12">
+        <div className="row">
+           <div className="col-12"><h1>Create Account</h1></div>
+           <div className="col-12">
             <form>
-              <div class="form-group d-flex justify-content-center">
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name" />
+              <div className="form-group d-flex justify-content-center">
+                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name" />
               </div>
-              <div class="form-group d-flex justify-content-center">
-                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Surname" />
+              <div className="form-group d-flex justify-content-center">
+                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Surname" />
               </div>
-              <div class="form-group d-flex justify-content-center">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail" />
+              <div className="form-group d-flex justify-content-center">
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail" />
               </div>
-              <div class="form-group d-flex justify-content-center">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+              <div className="form-group d-flex justify-content-center">
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
               </div>
-              <div class="form-group d-flex justify-content-center">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Repeat Password" />
+              <div className="form-group d-flex justify-content-center">
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Repeat Password" />
               </div>
-              <div><button type="submit" class="btn btn-primary">Create account</button></div>
+              <div><button type="submit" className="btn btn-primary">Create account</button></div>
              </form>
             </div>
         </div>
@@ -39,7 +39,3 @@ class Register extends React.Component {
   }
 }
 export default Register;
-    
-/* <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> */
